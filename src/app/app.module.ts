62a938bf0c8fd6991422513adeda73ad239f2cc1
VieiraLucasDuarte <108312353+VieiraLucasDuarte@users.noreply.github.com>
2024-05-13 22:38:38 +0000

    Adicionado novo menu lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { NavPrincipalComponent } from './components/nav-principal/nav-principal.component';
 import { ProdutosPrincipalComponent } from './components/produtos-principal/produtos-principal.component';
+import { MenuLateralPrincipal } from './components/menu-lateral-principal/menu-lateral-principal';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavPrincipalComponent,
-    ProdutosPrincipalComponent
+    ProdutosPrincipalComponent,
+    MenuLateralPrincipal
   ],
   imports: [
     BrowserModule,
