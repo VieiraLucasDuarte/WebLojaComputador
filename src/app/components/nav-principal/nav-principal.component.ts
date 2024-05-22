e96cd8cbcda363fef9c0ce528d50af295affa9bb
Lucas <lucasduartevieira0@gmail.com>
2024-05-21 21:17:15 -0300

    ✨: adicionado metodo de consultar categoria

--- src/app/components/nav-principal/nav-principal.component.ts
+++ src/app/components/nav-principal/nav-principal.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria } from 'src/app/model/categoria';
+import { CategoriaService } from 'src/app/service/categoria.service';
 
 @Component({
   selector: 'app-nav-principal',
   templateUrl: './nav-principal.component.html',
   styleUrls: ['./nav-principal.component.scss']
 })
-export class NavPrincipalComponent {
+export class NavPrincipalComponent implements OnInit {
 
+  categorias: Categoria[] = []
+
+  constructor(
+    private categoriaService: CategoriaService,
+  ) { }
+
+
+  ngOnInit() {
+    this.getCategoria();
+
+  }
+
+  private getCategoria() {
+    this.categoriaService.getCategorias()
+      .subscribe(item =>
+        this.categorias = item,
+      );
+  }
+
+  getProdutoByFiltro() {
+    ///repsensar legal
+
+    //rever como passa imagem com h2
+  }
 }
