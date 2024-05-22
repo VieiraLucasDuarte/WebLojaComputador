a69984a31e8219c2a4fe09dcef3aa200de093113
VieiraLucasDuarte <108312353+VieiraLucasDuarte@users.noreply.github.com>
2024-05-22 16:08:40 +0000

    ✨: Adicionado dropDown ao navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ProdutosPrincipalComponent } from './components/produtos-principal/prod
 import { MenuLateralPrincipal } from './components/menu-lateral-principal/menu-lateral-principal';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -26,6 +29,10 @@ import {MatButtonModule} from '@angular/material/button';
     HttpClientModule,
     MatButtonModule,
     MatMenuModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/nav-principal/nav-principal.component.ts
+++ src/app/components/nav-principal/nav-principal.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Categoria } from 'src/app/model/categoria';
 import { CategoriaService } from 'src/app/service/categoria.service';
 
 @Component({
   selector: 'app-nav-principal',
   templateUrl: './nav-principal.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./nav-principal.component.scss']
 })
 export class NavPrincipalComponent implements OnInit {
 
-  categorias: Categoria[] = []
+  categorias: Categoria[] = [];
+  panelColor = new FormControl('red');
 
   constructor(
     private categoriaService: CategoriaService,
@@ -22,10 +25,34 @@ export class NavPrincipalComponent implements OnInit {
   }
 
   private getCategoria() {
-    this.categoriaService.getCategorias()
-      .subscribe(item =>
-        this.categorias = item,
-      );
+    this.mockTeste();
+    // this.categoriaService.getCategorias()
+    //   .subscribe(item =>
+    //     this.categorias = item,
+    //   );
+
+
+  }
+
+  private mockTeste() {
+    const categorias: Categoria[] = [
+      {
+          id: 1,
+          descricao: 'Tecnologia',
+          ativo: true
+      },
+      {
+          id: 2,
+          descricao: 'Educação',
+          ativo: true
+      },
+      {
+          id: 3,
+          descricao: 'Saúde',
+          ativo: false
+      }
+  ];
+  this.categorias = categorias
   }
 
   getProdutoByFiltro() {
